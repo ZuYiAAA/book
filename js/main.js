1ba1aff694c86dd0ae53d77001d4cdddde71f9e7
ZuYiAAA <526100064@qq.com>
2021-01-15 17:13:21 +0800

    111

--- js/main.js
+++ js/main.js
@@ -139,36 +139,36 @@ function loading() {
                         // Auto center this flipbook
                         autoCenter: true,
                         when: {
-                            turning: function (e, page, view) {
-                                if (page == 1) {
-                                    // $(".btnImg").css("display", "none");
-                                    $(".mark").css("display", "block");
-                                } else {
-                                    // $(".btnImg").css("display", "block");
-                                    $(".mark").css("display", "none");
-                                }
-                                if (page == 33) {
-                                    $(".nextPage").css("display", "none");
-                                } else {
-                                    $(".nextPage").css("display", "block");
-                                }
-                            },
-                            turned: function (e, page, view) {
-                                console.log(page);
-                                var total = $(".flipbook").turn("pages");//总页数
-                                if (page == 1) {
-                                    $(".return").css("display", "none");
-                                    // $(".btnImg").css("display", "none");
-                                } else {
-                                    $(".return").css("display", "block");
-                                    // $(".btnImg").css("display", "block");
-                                }
-                                if (page == 2) {
-                                    $(".catalog").css("display", "block");
-                                } else {
-                                    $(".catalog").css("display", "none");
-                                }
-                            }
+                            // turning: function (e, page, view) {
+                            //     if (page == 1) {
+                            //         // $(".btnImg").css("display", "none");
+                            //         $(".mark").css("display", "block");
+                            //     } else {
+                            //         // $(".btnImg").css("display", "block");
+                            //         $(".mark").css("display", "none");
+                            //     }
+                            //     if (page == 33) {
+                            //         $(".nextPage").css("display", "none");
+                            //     } else {
+                            //         $(".nextPage").css("display", "block");
+                            //     }
+                            // },
+                            // turned: function (e, page, view) {
+                            //     console.log(page);
+                            //     var total = $(".flipbook").turn("pages");//总页数
+                            //     if (page == 1) {
+                            //         $(".return").css("display", "none");
+                            //         // $(".btnImg").css("display", "none");
+                            //     } else {
+                            //         $(".return").css("display", "block");
+                            //         // $(".btnImg").css("display", "block");
+                            //     }
+                            //     if (page == 2) {
+                            //         $(".catalog").css("display", "block");
+                            //     } else {
+                            //         $(".catalog").css("display", "none");
+                            //     }
+                            // }
                         }
                     })
                 }
